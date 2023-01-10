86604c6094680c22497d978d92b371e66e175177
Rkulsak <78222112+Rkulsak@users.noreply.github.com>
2023-01-09 20:17:22 -0500

    hi

--- CreativeCoding/p5/GridGif/sketch.js
+++ CreativeCoding/p5/GridGif/sketch.js
@@ -12,12 +12,21 @@ function draw() {
   fill(255,0,0);
   strokeWeight(1);
 
-  var numSquares=30;
-  var sideLen=windowWidth/numSquares;
-  for (var x=0;x<numSquares*numSquares;x+=1){ //this loop creates a row in the x direction
-    var yCounter = int(x/numSquares);
-    image(gif,(x%numSquares)*sideLen,yCounter*sideLen,sideLen,sideLen);
+  // var numSquares=30;
+  // var sideLen=windowWidth/numSquares;
+  // for (var x=0;x<numSquares*numSquares;x+=1){ //this loop creates a row in the x direction
+  //   var yCounter = int(x/numSquares);
+  //   image(gif,(x%numSquares)*sideLen,yCounter*sideLen,sideLen,sideLen);
+  //
+  // }
 
+  var num=30;
+  var sideLen=windowWidth/num;
+
+  for (var y = 0; y<windowWidth;y+=sideLen){
+    for(var x = 0;x<windowWidth;x+=sideLen){
+      image(gif,x,y,sideLen,sideLen);
+    }
   }
 
 
