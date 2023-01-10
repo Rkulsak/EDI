7f4c70ea1b3f62cb498962ed71b62ddcbd8204ee
Rkulsak <78222112+Rkulsak@users.noreply.github.com>
2023-01-10 13:30:33 -0500

    hi

---
+++ CreativeCoding/p5/gridGifFinal/sketch.js
@@ -0,0 +1,36 @@
+let gif;
+
+function preload(){
+  gif=loadImage("assets/graphicGIF.gif");
+}
+function setup() {
+  createCanvas(windowWidth,windowWidth);
+}
+
+function draw() {
+  fill(255,0,0);
+  strokeWeight(1);
+  background(255,255,0);
+
+
+  // var numSquares=30;
+  // var sideLen=windowWidth/numSquares;
+  // for (var x=0;x<numSquares*numSquares;x+=1){ //this loop creates a row in the x direction
+  //   var yCounter = int(x/numSquares);
+  //   image(gif,(x%numSquares)*sideLen,yCounter*sideLen,sideLen,sideLen);
+  //
+  // }
+  var num=30;
+  var sideLen=windowWidth/num;
+  translate(-150,150);
+  for (var y = 0; y<windowWidth;y+=sideLen){
+    for(var x = 0;x<windowWidth;x+=sideLen){
+      image(gif,x,y,sideLen,sideLen);
+    }
+  }
+
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+}
