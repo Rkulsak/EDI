ecda7fce89acf45752c830b473be1f4ebc0d2e33
Rkulsak <78222112+Rkulsak@users.noreply.github.com>
2023-01-10 13:07:21 -0500

    Update sketch.js

--- CreativeCoding/p5/variables/sketch.js
+++ CreativeCoding/p5/variables/sketch.js
@@ -1,23 +1,38 @@
+let gif;
+
+function preload(){
+  gif=loadImage("assets/graphicGIF.gif");
+}
 function setup() {
-  createCanvas(720,720);
-  background(255,255,0);
+  createCanvas(windowWidth,windowWidth);
 }
 
 function draw() {
   fill(255,0,0);
   strokeWeight(1);
-  for (var x=0;x<4;x+=1){ //this loop creates a row in the x direction
-    if(x<2){
-      square(x*360,0,360);
-    }
-    else {
-      square((x-2)*360,360,360);
+  background(255,255,0);
+
+
+  // var numSquares=30;
+  // var sideLen=windowWidth/numSquares;
+  // for (var x=0;x<numSquares*numSquares;x+=1){ //this loop creates a row in the x direction
+  //   var yCounter = int(x/numSquares);
+  //   image(gif,(x%numSquares)*sideLen,yCounter*sideLen,sideLen,sideLen);
+  //
+  // }
+  translate(-150,150);
+  var num=30;
+  var sideLen=windowWidth/num;
+
+  for (var y = 0; y<windowWidth;y+=sideLen){
+    for(var x = 0;x<windowWidth;x+=sideLen){
+      image(gif,x,y,sideLen,sideLen);
     }
   }
 
+}
 
 
-
-
-
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight)
 }
